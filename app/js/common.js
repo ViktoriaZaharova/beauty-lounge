5db2c6a980d8bfbdf9b51f55e92f50f36da2beaf
viktoriazaharova <zaharova181114@gmail.com>
2020-12-20 21:10:30 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    var penImg = $('.gallery-item img');
-    var linksImg = $('.gallery-item');
+    let penImg = $('.gallery-item img');
+    let linksImg = $('.gallery-item');
 
     $('.gallery-min').on('click', function (e) {
-        var imgPath;
+        let imgPath;
 
         imgPath = $(this).attr('data-img-path');
 
@@ -24,14 +24,14 @@ $('.btn-close').click(function (){
 
 // модальные окна (несколько)
 $(document).ready(function () {
-    var overlay = $('.overlay');
-    var open_modal = $('.open_modal');
-    var close = $('.modal__close, .overlay');
-    var modal = $('.modal__div');
+    let overlay = $('.overlay');
+    let open_modal = $('.open_modal');
+    let close = $('.modal__close, .overlay');
+    let modal = $('.modal__div');
 
     open_modal.click(function (event) {
         event.preventDefault();
-        var div = $(this).attr('href');
+        let div = $(this).attr('href');
         overlay.fadeIn(400,
             function () {
                 $(div)
@@ -57,3 +57,21 @@ $(document).ready(function () {
     });
 });
 //end
+
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 768) {
+        $('.articles-slider:not(.slick-initialized)').slick({
+            dots: false,
+            infinite: true,
+            slidesToShow: 1,
+            arrows: false,
+            variableWidth: true,
+            autoplay: true
+        });
+    } else {
+        $(".articles-slider.slick-initialized").slick("unslick");
+    }
+});
+// slick active
